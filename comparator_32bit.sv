be858f650e7f21056883201183d4cc26bdaf7e64
Vntdat <vngtdat@gmail.com>
2025-03-28 15:18:58 +0700

    SLT

--- comparator_32bit.sv
+++ comparator_32bit.sv
@@ -1,9 +1,7 @@
 module comparator_32bit (
     input logic [31:0] a,
     input logic [31:0] b,
-    output logic A_eq_B,  // a == b
-    output logic A_lt_B,  // a < b
-    output logic A_gt_B   // a > b
+    output logic A_lt_B  // a < b
 );
 
     logic [31:0] sub_result;
@@ -23,10 +21,7 @@ module comparator_32bit (
     );
 
     // Kiểm tra các điều kiện
-    assign A_eq_B = (sub_result == 0);  // Nếu hiệu bằng 0 thì a == b
     assign A_lt_B = (a_sign & ~b_sign) |  // Nếu a âm và b dương thì a < b
                     (~(a_sign ^ b_sign) & ~c_out);  // Nếu cùng dấu và c_out = 0 thì a < b
-    assign A_gt_B = (b_sign & ~a_sign) |  // Nếu b âm và a dương thì a > b
-                    (~(a_sign ^ b_sign) & c_out);  // Nếu cùng dấu và c_out = 1 thì a > b
 
 endmodule
