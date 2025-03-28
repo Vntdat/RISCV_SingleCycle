bc980f3bd63a845b3b87da374527ad9005833187
Vntdat <vngtdat@gmail.com>
2025-03-28 18:24:35 +0700

    SLTU

--- comparator_32bit_unsigned.sv
+++ comparator_32bit_unsigned.sv
@@ -1,18 +1,18 @@
 module comparator_32bit_unsigned(
-    input logic [31:0] rs1, rs2,    // Hai số cần so sánh
-    output logic rs1_lt_rs2      // rs1 < rs2
+    input logic [31:0] a, b,    // Hai số cần so sánh
+    output logic a_lt_b         // a < b
 );
     logic [31:0] sub_result;
     logic borrow;
 
     // Sử dụng bộ trừ 32-bit
     sub_32 subtractor (
-        .a(rs1),
-        .b(rs2),
+        .a(a),
+        .b(b),
         .s(sub_result),
-        .c_out(borrow) // borrow = 0 nếu A < B
+        .c_out(borrow) // borrow = 0 nếu a < b
     );
 
-    assign A_lt_B = ~borrow;               // Nếu có mượn (borrow = 0) thì A < B
+    assign a_lt_b = ~borrow;  // Nếu có mượn (borrow = 0) thì a < b
 
 endmodule
