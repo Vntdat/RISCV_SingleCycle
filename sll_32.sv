9cd0f0478be567b0006f727a7ce8f67e3e6f3a7a
Vntdat <vngtdat@gmail.com>
2025-03-28 18:25:08 +0700

    SLL

--- sll_32.sv
+++ sll_32.sv
@@ -1,15 +1,15 @@
 module sll_32 (
-	input  logic [31:0] rs1,    // Giá trị đầu vào cần dịch
-	input  logic [31:0] rs2,   // Số bit dịch (5-bit)
-	output logic [31:0] rd      // Kết quả sau khi dịch
-	);
-	logic [31:0] stage1, stage2, stage3, stage4, stage5;
-	logic [4:0] shift;
-	
-	assign shift = rs2[4:0];
-	
+    input  logic [31:0] a,    // Giá trị đầu vào cần dịch
+    input  logic [31:0] b,    // Số bit dịch (5-bit)
+    output logic [31:0] c     // Kết quả sau khi dịch
+);
+    logic [31:0] stage1, stage2, stage3, stage4, stage5;
+    logic [4:0] shift;
+    
+    assign shift = b[4:0];
+
     // Tầng dịch 16-bit
-    assign stage1 = shift[4] ? {rs1[15:0], 16'b0} : rs1;
+    assign stage1 = shift[4] ? {a[15:0], 16'b0} : a;
 
     // Tầng dịch 8-bit
     assign stage2 = shift[3] ? {stage1[23:0], 8'b0} : stage1;
@@ -24,6 +24,6 @@ module sll_32 (
     assign stage5 = shift[0] ? {stage4[30:0], 1'b0} : stage4;
 
     // Kết quả cuối cùng
-    assign rd = stage5;
+    assign c = stage5;
 
 endmodule
