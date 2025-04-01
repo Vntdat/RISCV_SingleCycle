748db0f9893b973f685d37cd23c252ae2d967647
Vntdat <vngtdat@gmail.com>
2025-04-01 08:16:19 +0700

    SLT

--- comparator_32bit.sv
+++ comparator_32bit.sv
@@ -5,7 +5,7 @@ module comparator_32bit (
 );
 
     logic [31:0] sub_result;
-    logic c_out;
+    logic cout;
     logic a_sign, b_sign;
 
     // Lấy bit dấu của hai số
@@ -17,11 +17,11 @@ module comparator_32bit (
         .a(a),
         .b(b),
         .s(sub_result),
-        .c_out(c_out)
+        .cout(cout)
     );
 
     // Kiểm tra các điều kiện
     assign A_lt_B = (a_sign & ~b_sign) |  // Nếu a âm và b dương thì a < b
-                    (~(a_sign ^ b_sign) & ~c_out);  // Nếu cùng dấu và c_out = 0 thì a < b
+                    (~(a_sign ^ b_sign) & ~cout);  // Nếu cùng dấu và c_out = 0 thì a < b
 
 endmodule
