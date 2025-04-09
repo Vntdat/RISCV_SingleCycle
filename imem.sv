7d280324c1d45ce66cd0014a1fe069d741d9b068
Suerete <133035428+Suerete@users.noreply.github.com>
2025-04-10 01:19:13 +0700

    Update imem.sv

--- imem.sv
+++ imem.sv
@@ -1,14 +1,14 @@
 module imem (
-	input logic [31:0] imem_addr,
-	output logic [31:0] imem_data
-	);
-	
-	logic [31:0] imem [0:63];
-	initial begin
-		$readmemh ("D:/HCMUT/HK242/CTMT/test/isa.mem", imem);
-	end
-		
-	always_comb begin	
-		imem_data = imem[imem_addr[7:2]];
-	end
+    input  logic [31:0] i_addr,
+    output logic [31:0] o_inst
+);
+    logic [31:0] memory [0:8191];
+    initial begin
+        $readmemh("E:/BKHCM/HK 242/CTMT/singlecycle/isa.mem", memory);  // file imem.mem chứa mã lệnh hex
+    end
+    always_comb begin
+        o_inst = memory[i_addr[14:2]];
+    end
+
 endmodule
+
